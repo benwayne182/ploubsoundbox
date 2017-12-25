66840c6c5e43a0949320f9b6075f54940330f9de
benwayne182 <benwayne182@gmail.com>
2017-12-26 00:37:21 +0100

    Add some javadoc

--- app/src/main/java/com/fdp/benwayne/ploubsoundbox/MainActivity.java
+++ app/src/main/java/com/fdp/benwayne/ploubsoundbox/MainActivity.java
@@ -11,10 +11,53 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    /**
+     * Message d'ouverture
+     */
     private TextView textView;
+    /**
+     * Change de saisie du nom d'utilisateur
+     */
     private EditText editText;
-    private Button button;
+    /**
+     * Bouton start
+     */
+    private Button startButton;
+    /**
+     * Nom de l'utilisateur
+     */
     private String userName;
+    /**
+     * Listener sur la modification du champs de saisie
+     */
+    private TextWatcher textWatcher = new TextWatcher() {
+
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+        }
+
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+            boolean b = (s.length() != 0);
+            startButton.setEnabled(b);
+            userName = b ? s.toString() : "";
+        }
+
+        @Override
+        public void afterTextChanged(Editable s) {
+
+        }
+    };
+    /**
+     * Listener sur le clic du bouton start
+     */
+    private View.OnClickListener onClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            startSoundbox(v);
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,30 +65,15 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         this.textView = this.findViewById(R.id.activityMainTextView);
         this.editText = this.findViewById(R.id.activityMainEditText);
-        this.button = this.findViewById(R.id.activityMainStartButton);
+        this.startButton = this.findViewById(R.id.activityMainStartButton);
 
-        this.button.setEnabled(false);
-        TextWatcher textWatcher = new TextWatcher() {
+        this.startButton.setEnabled(false);
 
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-
-            }
-
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-                button.setEnabled(s.length() != 0);
-            }
-
-            @Override
-            public void afterTextChanged(Editable s) {
-
-            }
-        };
-        this.editText.addTextChangedListener(textWatcher);
+        this.editText.addTextChangedListener(this.textWatcher);
+        this.startButton.setOnClickListener(this.onClickListener);
     }
 
-    public void startSoundbox(View view) {
+    protected void startSoundbox(View view) {
 
     }
 }
